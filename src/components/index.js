4e556a5d5ca9e6ab0c0f2fa4ab7e7829d35316f2
YongPilMoon <m71661@gmail.com>
2017-07-16 23:24:33 +0900

    Add Authentication component

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navigation from "./Navigation";
+import PostList from "./PostList";
+import PostDetail from "./PostDetail";
+import PostCreate from "./PostCreate";
+import PostEdit from "./PostEdit";
+import Authentication from "./Authentication";
+
+export {Navigation, PostList, PostDetail, PostCreate, PostEdit, Authentication}
