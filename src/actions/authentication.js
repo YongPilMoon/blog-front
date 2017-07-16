a8faac7f82c4bc6890831f30e81f1e6962881dd4
YongPilMoon <m71661@gmail.com>
2017-07-16 23:19:12 +0900

    Add login actions

---
+++ src/actions/authentication.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import config from '../../config/config';
+
+import {
+    AUTH_LOGIN,
+    AUTH_LOGIN_SUCCESS,
+    AUTH_LOGIN_FAILURE
+} from './ActionTypes';
+
+export function loginRequest(username, password) {
+    return (dispatch) => {
+        dispatch(login());
+        return axios.post(`${config.API_URL}/user/login`, {
+            username: username,
+            password:password
+        }).then((response) => {
+            let token = response.data.token;
+            dispatch(loginSuccess(username, token));
+            }).catch((error) => {
+            dispatch(loginFailure());
+            });
+    };
+}
+
+export function login() {
+    return {
+        type: AUTH_LOGIN
+    };
+}
+
+export function loginSuccess(username, token) {
+    return {
+        type: AUTH_LOGIN_SUCCESS,
+        username,
+        token
+    };
+}
+
+export function loginFailure() {
+    return {
+        type: AUTH_LOGIN_FAILURE
+    };
+}
