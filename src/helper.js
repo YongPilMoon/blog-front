76aef9eaff9609f86fde8b6e0bd82d7fe0d45357
YongPilMoon <m71661@gmail.com>
2017-07-17 22:47:04 +0900

    Add cookie helper functions

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,32 @@
-const changeDateFormat = (date) => {
+export const changeDateFormat = (date) => {
     const dateObj = new Date(date);
     return `${dateObj.getFullYear()}년 ${dateObj.getMonth()+1}월 ${dateObj.getDate()}일`
 };
 
-export default changeDateFormat
+export function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+export function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+export function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+
 
